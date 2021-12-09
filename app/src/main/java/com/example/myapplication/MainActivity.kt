e7b85929141a6bd652cab565c50b6aabd152b939
Tom <chhan@maxst.com>
2021-12-09 17:34:41 +0900

    compose 관련 layout 및 버튼 관련 예제 샘플 테스트

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,73 +3,65 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
 
-	private var list: ArrayList<Int> = arrayListOf()
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-
-		list.add(0)
-		list.add(1)
-		list.add(2)
-		list.add(3)
-		list.add(4)
 		setContent {
 			MyApplicationTheme {
-				// A surface container using the 'background' color from the theme
-				Surface(color = MaterialTheme.colors.background) {
-					boxsemple()
-				}
+				MyApp()
 			}
 		}
 	}
 }
-@Composable
-fun boxsemple() {
-	Box {
-		var imageHeightPx by remember { mutableStateOf(0) }
 
-		Image(
-			painter = painterResource(R.drawable.ic_launcher_background),
-			contentDescription = "I'm above the text",
-			modifier = Modifier
-				.fillMaxWidth()
-				.onSizeChanged { size ->
-					// Don't do this
-					imageHeightPx = size.height
-				}
-		)
-		Column {
-			Text(
-				text = "I'm below the image",
-				modifier = Modifier.padding(
-					top = with(LocalDensity.current) { imageHeightPx.toDp() }
-				)
-			)
-			Text(
-				text = "I'm below the image",
-				modifier = Modifier.padding(
-					top = with(LocalDensity.current) { imageHeightPx.toDp() }
-				),
-				color = Color.Cyan
-			)
+@Composable
+fun MyApp(names: List<String> = listOf("World", "Compose")) {
+	Column(modifier = Modifier.padding(vertical = 4.dp)) {
+		for (name in names) {
+			Greeting(name = name)
+		}
+	}
+}
+
+@Composable
+private fun Greeting(name: String) {
+	var expanded = remember { mutableStateOf(false) }
+	Surface(
+		color = MaterialTheme.colors.primary,
+		modifier = Modifier
+			.padding(vertical = 4.dp, horizontal = 8.dp)
+	) {
+		Row(modifier = Modifier.padding(24.dp)) {
+			Column(
+				modifier = Modifier.weight(1f)
+			) {
+				Text(text = "Hello, ")
+				Text(text = name)
+			}
+			OutlinedButton(
+				onClick = {
+					expanded.value = !expanded.value
+				},
+			) {
+				Text(if (expanded.value) "Show less" else "Show more")
+			}
 		}
 	}
 }
@@ -78,6 +70,6 @@ fun boxsemple() {
 @Composable
 fun DefaultPreview() {
 	MyApplicationTheme {
-		boxsemple()
+		MyApp()
 	}
 }
